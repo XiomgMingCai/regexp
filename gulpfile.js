82fbc0026325fbf3252647dbb87b3b4958ee8926
xiongMingCai <xmc000@icloud.com>
2018-01-11 10:17:54 +0800

    add gulp-gh-pages

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xiongMingCai on 2018/1/11.
+ */
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages());
+});
